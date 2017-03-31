77715919c487c0b4335306b29c1f2ea61ae31644
nikk1210 <ashok.kadu102@gmail.com>
2017-03-31 18:00:03 +0530

    New files are added in repository

---
+++ numbertable.java
@@ -0,0 +1,11 @@
+class numbertable {
+public static void main(String[] args) {
+System.out.println("Table for 2");
+int product;
+for(int i=1; i<=10; i++) {
+product=2*i;
+System.out.println("2x" +i+": "+product);
+}
+System.out.println("Table printed successfully !!");
+}
+}
